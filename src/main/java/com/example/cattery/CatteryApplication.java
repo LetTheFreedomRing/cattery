f7986e7ba9bd4382188a537999f9217225ebda56
Pavlo Kolesnichenko <pavlo.kolesnichenko@gmail.com>
2020-06-09 10:14:04 +0300

    Initial commit

---
+++ src/main/java/com/example/cattery/CatteryApplication.java
@@ -0,0 +1,13 @@
+package com.example.cattery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatteryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatteryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cattery/CatteryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cattery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatteryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
